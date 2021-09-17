d0d04d53b92c50a6f5a351fede128c0d60de1adc
OmarO <omar_orejuela@hotmail.com>
2021-09-16 20:53:18 -0500

    Inclusión de caracteristicas

---
+++ Get.js
@@ -0,0 +1,5 @@
+var getData = function() {
+    var name = document.getElementById("usuario").value;
+    var pass = document.getElementById("clave").value;
+    console.log(name+"este"+pass);
+}
---
+++ formulario.php
@@ -0,0 +1,5 @@
+<?php
+/* Muestra los datos enviados a través de un formulario HTML
+a través del estado actual de PHP */
+phpinfo( );
+?>
