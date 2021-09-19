7649abd12bf2f3e4b71941cebf2feed43fc1c322
OmarO <omar_orejuela@hotmail.com>
2021-09-18 20:08:07 -0500

    Actualizacion PHP

--- recibir.php
+++ recibir.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 $usuario = $_POST["usuario"];
 $clave = $_POST['clave'];
@@ -7,7 +6,4 @@ $clave = $_POST['clave'];
 echo "Hola ", $usuario;
 echo "<br>";
 echo "Tu clave fue: ", $clave;
-=======
-echo "Recibe datos";
->>>>>>> 29349ffb888ad347921a96aa7a2ec652b10f9ce0
 ?>
